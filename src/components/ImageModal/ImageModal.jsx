6c56eb8ca6f80d9af82ff5fda79b479fd745eba1
Alex <159346450+AlexandraSavenko@users.noreply.github.com>
2024-08-31 06:41:01 +0300

    some styles

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -23,10 +23,33 @@ export default function ImageModal({ isOpen, onClose, modalData }) {
       shouldCloseOnEsc={true}
       shouldCloseOnOverlayClick={true}
       overlayClassName={css.overlay}
+      className={css.modal}
+      style={{
+        overlay: {
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundColor: "rgba(0, 0, 0, 0.712)",
+        },
+        content: {
+          position: "absolute",
+          top: "40px",
+          left: "40px",
+          right: "40px",
+          bottom: "40px",
+          // overflow: "auto",
+          WebkitOverflowScrolling: "touch",
+          borderRadius: "4px",
+          outline: "none",
+        },
+      }}
     >
-      <div className={css.mwrap}>
+      <div>
         {modalData ? (
           <img
+            className={css.image}
             src={modalData && modalData.urls.regular}
             alt={modalData && modalData.urls.alt_description}
           />
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,15 +2,17 @@ import { ThreeDots } from "react-loader-spinner";
 import css from "./Loader.module.css";
 export default function Loader() {
   return (
-    <ThreeDots
-      visible={true}
-      height="80"
-      width="80"
-      color="#4fa94d"
-      radius="9"
-      ariaLabel="three-dots-loading"
-      wrapperStyle={{}}
-      wrapperClass=""
-    />
+    <div className={css.Loader}>
+      <ThreeDots
+        visible={true}
+        height="80"
+        width="80"
+        color="#4fa94d"
+        radius="9"
+        ariaLabel="three-dots-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+      />
+    </div>
   );
 }
